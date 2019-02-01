8df673f60eba8b6220f6e406b0276e40142c4020
Bogdan Petrenko <pebogdan@ukr.net>
2019-02-01 10:15:14 +0200

    url_parser is working without tld..

--- url_parser.php
+++ url_parser.php
@@ -1,13 +1,34 @@
 <?php
 
+function mainMovie(string $url) : void
+{
+    $parsedUrl = parse_url($url);
+    parse_str($parsedUrl['host'], $parsedHost);
+
+    echo json_encode($parsedUrl, JSON_PRETTY_PRINT) . PHP_EOL;
+
+    if ( isset( $parsedUrl['query'])) {
+        parse_str($parsedUrl['query'], $parsedQuery);
+        echo ('{' . PHP_EOL . 'parsedQuery;' .
+            json_encode($parsedQuery, JSON_PRETTY_PRINT) . PHP_EOL .
+        '}' . PHP_EOL);
+    }
+}
+
 $shortopts = "u:";
 $longopts = ['url:'];
-
-$options = getopt($shortopts, $longopts);
 $argv;
 
-print_r($options);
-print_r($argv);
-$result = filter_var($options, FILTER_VALIDATE_URL);
+$options = getopt($shortopts, $longopts);
 
-print_r($result);
+switch (TRUE) {
+    case isset($options['u']):
+        filter_var($options['u'], FILTER_VALIDATE_URL) ? mainMovie($options['u']) : NULL;
+        break;
+    case isset($options['url']):
+        filter_var($options['url'], FILTER_VALIDATE_URL) ? mainMovie($options['url']) : NULL;
+        break;
+    case isset($argv[1]):
+        filter_var($argv[1], FILTER_VALIDATE_URL) ? mainMovie($argv[1]) : NULL;
+        break;
+}
